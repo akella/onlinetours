3534e6d4ad459147ad64321bc185a4459eda3b57
hraboviyvadim <hraboviyvadim@gmail.com>
2013-06-15 16:50:36 +0300

    faq popup added

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+// options popup
 	$(".options__want-same").click(function(){
 			$(".options-popup").slideDown();
 		return false;
@@ -7,5 +8,18 @@ $(document).ready(function() {
 			$(".options-popup").slideUp();
 		return false;
 	});	
+// faq popup
+	$(".order-btn_faq").click(function(){
+			$(".faq-popup").slideDown();
+		return false;
+	});
+	$(".faq-popup__close").click(function(){
+			$(".faq-popup").slideUp();
+		return false;
+	});
+
+
+
+
 });
 
